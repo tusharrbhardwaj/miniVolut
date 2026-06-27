38db4ee9dcd78dba7605ea7273713bc675a86fd0
Tushar Bhadwaj <tusharrbhardwaj@gmail.com>
2026-06-27 04:31:14 +0200

    leanred the role of routes

--- app.py
+++ app.py
@@ -2,9 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Hello world"
+
+    return """
+    <h1>MiniVault</h1>
+
+    <p>Hello Tushar</p>
+    """
+
+@app.route("/about")
+def about():
+    return "I'm a software Engineering student"
+
+@app.route("/contact")
+def contact():
+    return "You can contact me at - tusharrbhardwaj@gmail.com"
 
 
 if __name__ == "__main__":
